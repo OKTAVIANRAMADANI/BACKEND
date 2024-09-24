936a7c65deb377ea967e46700d1177a2fc6a1306
OKTAVIANRAMADANI <161546343+OKTAVIANRAMADANI@users.noreply.github.com>
2024-09-24 10:13:51 +0700

    Add files via upload

---
+++ Pertemuan 1/latihan.py
@@ -0,0 +1,4 @@
+print("Hai apa kabar boloo")
+web="Danzz Kece"
+print("Sedang menjalankan tugas kuliah di  " + web)
+print("Yokk gasss!!")
---
+++ Pertemuan 1/latihan2.py
@@ -0,0 +1,6 @@
+web = "Danzz Kroco"
+print(web)
+harga = 6000
+print(harga)
+sukses = True
+print(sukses)
