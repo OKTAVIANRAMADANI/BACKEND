b95469150e0a3e04947e53742f6fd58b0813ae27
OKTAVIANRAMADANI <161546343+OKTAVIANRAMADANI@users.noreply.github.com>
2024-09-24 10:16:17 +0700

    Add files via upload

---
+++ Pertemuan 2/Latihan3.py
@@ -0,0 +1,10 @@
+x = 10
+y = 39
+
+print("Penambahan", x+y)
+print("Pengurangan ",x-y)
+print("Perkalihan ",x*y)
+print("Pembagian(dipecahkan) ",x/y)
+print("Pembagian(dibulatkan ke bawah) ",x//y)
+print("Modolus (sisa hasil dibagi) ",x%y)
+print("Pemangkatan ",x**y)
